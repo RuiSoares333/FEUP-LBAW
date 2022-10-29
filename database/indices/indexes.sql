f2e40862ec3e8adcba4c34f22bf7115508ee7294
Lucas Ferreira de Sousa <up202004682@fe.up.pt>
2022-10-29 18:55:56 +0100

    indexes
    Co-authored-by: Rui Soares <RuiSoares333@users.noreply.github.com>
    Co-authored-by: André Morais <Andrekt02@users.noreply.github.com>
    Co-authored-by: Joao Teixeira <joao-teixeira02@users.noreply.github.com>

--- database/indices/indexes.sql
+++ database/indices/indexes.sql
@@ -1,15 +1,16 @@
+SET search_path TO lbaw2286;
+
 CREATE INDEX news_comments ON comment USING hash (id_news);
 
 CREATE INDEX news_by_popularity ON news USING btree (reputation);
 
 CREATE INDEX user_notifications ON notification USING hash (id_user);
 
---FTS USERS
--- Add column to work to store computed ts_vectors.
+---------------------------FTS USERS--------------------------
+/*
 ALTER TABLE users
 ADD COLUMN tsvectors TSVECTOR;
 
--- Create a function to automatically update ts_vectors.
 CREATE FUNCTION users_search_update() RETURNS TRIGGER AS $$
 BEGIN
  IF TG_OP = 'INSERT' THEN
@@ -26,13 +27,71 @@ BEGIN
 END $$
 LANGUAGE plpgsql;
 
--- Create a trigger before insert or update on work.
 CREATE TRIGGER users_search_update
  BEFORE INSERT OR UPDATE ON users
  FOR EACH ROW
  EXECUTE PROCEDURE users_search_update();
 
-
--- Finally, create a GIN index for ts_vectors.
 CREATE INDEX search_username ON users USING GiST (tsvectors);
 
+
+----------------------------------FTS TAG-----------------------------------
+
+ALTER TABLE tag
+ADD COLUMN tsvectors TSVECTOR;
+
+CREATE FUNCTION tag_search_update() RETURNS TRIGGER AS $$
+BEGIN
+ IF TG_OP = 'INSERT' THEN
+        NEW.tsvectors = (
+         to_tsvector('english', NEW.tag_name));
+ END IF;
+ IF TG_OP = 'UPDATE' THEN
+         IF (NEW.tag_name <> OLD.tag_name) THEN
+           NEW.tsvectors = (
+            to_tsvector('english', NEW.tag_name));
+         END IF;
+ END IF;
+ RETURN NEW;
+END $$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER tag_search_update
+ BEFORE INSERT OR UPDATE ON tag
+ FOR EACH ROW
+ EXECUTE PROCEDURE tag_search_update();
+
+CREATE INDEX tag_search ON tag USING GIN (tsvectors);
+
+*/
+-------------------------------------------FTS NEWS------------------
+
+ALTER TABLE news
+ADD COLUMN tsvectors TSVECTOR;
+
+CREATE FUNCTION news_search_update() RETURNS TRIGGER AS $$
+BEGIN
+ IF TG_OP = 'INSERT' THEN
+        NEW.tsvectors = (
+         setweight(to_tsvector('english', NEW.title), 'A') ||
+         setweight(to_tsvector('english', NEW.content), 'B')
+        );
+ END IF;
+ IF TG_OP = 'UPDATE' THEN
+         IF (NEW.title <> OLD.title OR NEW.content <> OLD.content) THEN
+           NEW.tsvectors = (
+             setweight(to_tsvector('english', NEW.title), 'A') ||
+             setweight(to_tsvector('english', NEW.content), 'B')
+           );
+         END IF;
+ END IF;
+ RETURN NEW;
+END $$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER news_search_update
+ BEFORE INSERT OR UPDATE ON news
+ FOR EACH ROW
+ EXECUTE PROCEDURE news_search_update();
+
+CREATE INDEX search_news ON news USING GiST (tsvectors);
