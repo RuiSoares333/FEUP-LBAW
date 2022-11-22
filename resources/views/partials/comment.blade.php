4dac6c251fe0d5b64e9b997424d05affc3e79af3
up202005303 <up202005303@fe.up.pt>
2022-11-22 19:05:04 +0000

    show author of the comment

--- resources/views/partials/comment.blade.php
+++ resources/views/partials/comment.blade.php
@@ -1,6 +1,6 @@
 <article class="news container my-4 d-flex border bg-light" data-id="{{ $comment->id }}">
     <h4 class="my-auto">{{$comment -> content}}</h4>
-    <h4 class="my-auto">{{$comment -> user_id}}</h4>
+    <h4 class="my-auto">{{$comment->author()->get()->first()->username}}</h4>
     <div id="vote" class="fs-1 d-flex col-2 justify-content-end">
         <i class="bi bi-hand-thumbs-up"></i>
         <i class="bi bi-hand-thumbs-down"></i>
