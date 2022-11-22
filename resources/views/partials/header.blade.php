43a6bcc2c7d23182de51cf7364e0c7a69d6a5d94
JoaoTeixeira <joao.teixeira2924@gmail.com>
2022-11-22 15:46:17 +0000

    Search bar now in news feed only

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top text-uppercase">
     <div class="container px-5">
-        <div class="collapse navbar-collapse">
-            <form class="input-group my-3 h2">
-                <span class="input-group-text"><i class="bi bi-search"></i></span>
-                <input class="form-control text-light" type="search" placeholder="What are you looking for?" name="search" value="{{ request('search') }}">
-            </form>
-        </div>
         <h1><a href="{{ url('/') }}">slcn</a></h1>
         <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
