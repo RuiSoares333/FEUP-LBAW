d1f513355c1021e56fc57c4f0bd2906a1d6ba38d
Rui Soares <up202103631@fe.up.pt>
2022-12-19 16:27:11 +0000

    Edit Profile Re-arrange

--- resources/views/pages/edit_profile.blade.php
+++ resources/views/pages/edit_profile.blade.php
@@ -1,22 +1,31 @@
 @extends('layouts.app')
 @section('title', 'Edit Profile')
+
 @section('content')
 
-@include('partials.header')
+    @include('partials.header')
 
-@if(auth::check())
-<div id="form-newspost" class="my-5 row justify-content-center">
-    <form class="mt-2 edit_profile_username" method="POST" action="{{ route('edit_profile_api', ['id'=>$user->id]) }}" enctype="multipart/form-data">
-        <h1>Edit Profile</h1>
-        {{ csrf_field() }}
-        <input type="hidden" name="id" value="{{$user->id}}">
-        <input class="h2 fw-bold text-center " name="username" type="text" value="{{$user->username}}">  </h2>
-        <input class="h2 fw-bold text-center " name="country" type="text" value="{{$user->country}}"></h2>
-        <input class="h2 fw-bold text-center " name="email" type="text" value="{{$user->email}}"></h2>
-        <input class="h2 fw-bold text-center " name="password" type="password" placeholder="password"></h2>
-        <input type="file" name="picture" accept="image/png, image/jpeg" />
-        <input type="submit" value="Submit">
-    </form>
-@endif
+    @if(auth::check())
+    <div id="form-newspost" class="bubbles">
+        <form class="edit_profile_username static-form d-flex flex-column position-absolute text-center text-light" method="POST" action="{{ route('edit_profile_api', ['id'=>$user->id]) }}" enctype="multipart/form-data">
+            <h1 class="my-3">Edit Profile</h1>
+            {{ csrf_field() }}
+            <input type="hidden" name="id" value="{{$user->id}}">
+            <input class="fw-bold text-center" name="username" type="text" value="{{$user->username}}">  </h2>
+            <input class="fw-bold text-center" name="country" type="text" value="{{$user->country}}"></h2>
+            <input class="fw-bold text-center" name="email" type="text" value="{{$user->email}}"></h2>
+            <input class="fw-bold text-center" name="password" type="password" placeholder="password"></h2>
+            <input class="form-control w-75 mx-auto" type="file" id="formFile" accept="image/png, image/jpeg">
+            <input class="btn btn-primary fw-bold w-50 mx-auto" type="submit" value="Submit">
+        </form>
+    @endif
 
+@endsection
 
+@section('scripts')
+
+<script>
+    document.body.style.background = "linear-gradient(0deg, #bf6324 0%, #b92a4c 100%)";
+</script>
+
+@endsection
