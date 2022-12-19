399f185bc811f83577ad8ee6422820eaa2bf8269
up202005303 <up202005303@fe.up.pt>
2022-12-19 12:56:06 +0000

    tags next to news in news page

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+    protected $table = 'tag';
     public $timestamps = false;
 
     /**
@@ -20,8 +21,8 @@ class Tag extends Model
         return $this->belongsToMany('App\Models\User', 'tag_follow','id_user','id_tag')->orderBy('tag_name');
     }
 
-    public function news_tags() {
-        return $this->belongsToMany('App\Models\User', 'news_tag', 'id_news', 'id_tag');
+    public function news() {
+        return $this->belongsToMany('App\Models\News', 'news_tag', 'id_news', 'id_tag');
     }
 
     public function top_tags() {
--- resources/views/partials/newspost.blade.php
+++ resources/views/partials/newspost.blade.php
@@ -1,7 +1,10 @@
 <article class="news container my-5 d-flex flex-column border bg-light" data-id="{{ $newspost->id }}">
     <header class="m-3 d-flex flex-column">
         <h2 class="my-auto"><a href="/news/{{ $newspost->id }}">{{ $newspost->title }}</a></h2>
-        <h3>{{$newspost->news_tags()->get()}} </h3>
+        <h3> @foreach($newspost->tags as $tag)
+                Tags: {{ $tag->tag_name }}
+                @endforeach
+            </h3>
         <span class="my-auto"><a href="/profile/{{$newspost->author()->get()->first()->id}}">{{ $newspost->author()->get()->first()->username}}</a></span>
     </header>
     <div class="mx-3 d-flex flex-row"><p>{!! $newspost->content !!}</p></div>
