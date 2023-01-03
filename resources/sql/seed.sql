768d11949b59660dbb09734f9a1facf7c070b846
Rui Soares <up202103631@fe.up.pt>
2023-01-03 21:50:27 +0000

    Cleaning DB

--- resources/sql/seed.sql
+++ resources/sql/seed.sql
@@ -530,8 +530,8 @@ INSERT INTO users (id, username, email, password, country, is_admin) VALUES(1, '
 INSERT INTO users (id, username, email, password, country, is_admin) VALUES(2, 'João Teixeira', 'joao@legitmail.com', '$2a$12$z6QqR5X7k.JFeAK2UZAD6OdTgSj8Rkmf7sECS96dEGRzRjU/bhC.e', 'Portugal', true);
 INSERT INTO users (id, username, email, password, country, is_admin) VALUES(3, 'Lucas Sousa', 'lucas@legitmail.com', '$2a$12$lupa/IivieTHqeT8ZFuoc.b6Z4KpzOZ/LT6ts5Pxt9xq4c0y4vbti', 'Portugal', true);
 INSERT INTO users (id, username, email, password, country, is_admin) VALUES(4, 'Rui Soares', 'rui@legitmail.com', '$2a$12$hzvwGZBw2qWA6pE.v/JIPObYrX3odCi3bYwnm0XocZnpK.3Cv3wPC', 'Portugal', true);
-INSERT INTO users (id, username, email, password, country, is_admin) VALUES(5, '[redacted]', 'redac@legitmail.com', '$2a$12$gc7M2CUhl8QwvbsEkhaXW.aLeShBasRXwYPXqjiZoid/PrwZTvVCe', 'Zimbabue', false); --id 5 is deleted user
-INSERT INTO users (id, username, email, password, country, is_admin) VALUES(6, 'John Doe', 'admin@example.com', '$2y$10$HfzIhGCCaxqyaIdGgjARSuOKAcm1Uy82YfLuNaajn6JrjLWy9Sj/W', 'Doeee', true);
+INSERT INTO users (id, username, email, password, country, is_admin) VALUES(5, '[redacted]', 'redac@legitmail.com', '$2a$12$gc7M2CUhl8QwvbsEkhaXW.aLeShBasRXwYPXqjiZoid/PrwZTvVCe', '', false); --id 5 is deleted user
+INSERT INTO users (id, username, email, password, country, is_admin) VALUES(6, 'John Doe', 'admin@example.com', '$2y$10$HfzIhGCCaxqyaIdGgjARSuOKAcm1Uy82YfLuNaajn6JrjLWy9Sj/W', 'Chad', true);
 
 -------------------------------
 -- Follows
@@ -581,91 +581,6 @@ INSERT INTO tag(id, tag_name) VALUES (20, 'Music'); -- 20
 INSERT INTO tag(id, tag_name) VALUES (21, 'Anime'); -- 21
 
 -------------------------------
--- News
--------------------------------
-ALTER SEQUENCE news_id_seq RESTART WITH 5;
-
-INSERT INTO news (id, title, content, date, user_id) VALUES (1, 'Overwatch Fan Makes LEGO Bastion Figure for Their Brother', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod lacinia at quis risus sed vulputate odio ut.
-Dignissim convallis aenean et tortor. Eu feugiat pretium nibh ipsum consequat nisl. Interdum consectetur libero id faucibus.
-Erat velit scelerisque in dictum non consectetur a.', '2022.10.20', 1);
-
-INSERT INTO news (id, title, content, date, user_id) VALUES (2, 'Here’s What to Expect from Season 3 of The Witcher', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod lacinia at quis risus sed vulputate odio ut.
-Dignissim convallis aenean et tortor. Eu feugiat pretium nibh ipsum consequat nisl. Interdum consectetur libero id faucibus.
-Erat velit scelerisque in dictum non consectetur a.', '2022.10.20', 2);
-
-INSERT INTO news (id, title, content, date, user_id) VALUES (3, 'The State Of Destiny 2s Festival Of The Lost Is Unacceptable','Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod lacinia at quis risus sed vulputate odio ut.
-Dignissim convallis aenean et tortor. Eu feugiat pretium nibh ipsum consequat nisl. Interdum consectetur libero id faucibus.
-Erat velit scelerisque in dictum non consectetur a.', '2022.10.20', 3);
-
-INSERT INTO news (id, title, content, date, user_id) VALUES (4, 'Bleach TYBW shocks fans with brutal character deaths in episode 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod lacinia at quis risus sed vulputate odio ut.
-Dignissim convallis aenean et tortor. Eu feugiat pretium nibh ipsum consequat nisl. Interdum consectetur libero id faucibus.
-Erat velit scelerisque in dictum non consectetur a.', '2022.10.20', 4);
-
--------------------------------
--- news_favorite
--------------------------------
-INSERT INTO news_favorite(id_user, id_news) VALUES (1, 1);
-INSERT INTO news_favorite(id_user, id_news) VALUES (2, 2);
-INSERT INTO news_favorite(id_user, id_news) VALUES (3, 4);
-INSERT INTO news_favorite(id_user, id_news) VALUES (4, 3);
-
--------------------------------
--- news_vote
--------------------------------
-INSERT INTO news_vote(id_user, id_news, is_liked) VALUES (1, 1, true);
-INSERT INTO news_vote(id_user, id_news, is_liked) VALUES (1, 2, false);
-INSERT INTO news_vote(id_user, id_news, is_liked) VALUES (2, 2, true);
-INSERT INTO news_vote(id_user, id_news, is_liked) VALUES (2, 3, false);
-INSERT INTO news_vote(id_user, id_news, is_liked) VALUES (3, 1, true);
-INSERT INTO news_vote(id_user, id_news, is_liked) VALUES (3, 4, false);
-
--------------------------------
--- news_tag
--------------------------------
-INSERT INTO news_tag (id_news, id_tag) VALUES (1, 1); -- gaming
-INSERT INTO news_tag (id_news, id_tag) VALUES (2, 9); -- TV
-INSERT INTO news_tag (id_news, id_tag) VALUES (2, 7); -- Celebrities
-INSERT INTO news_tag (id_news, id_tag) VALUES (2, 1); -- Gaming
-INSERT INTO news_tag (id_news, id_tag) VALUES (3, 1); -- Gaming
-INSERT INTO news_tag (id_news, id_tag) VALUES (4, 8); -- Movies
-INSERT INTO news_tag (id_news, id_tag) VALUES (4, 9); -- TV
-INSERT INTO news_tag (id_news, id_tag) VALUES (4, 21); -- Anime
-
--------------------------------
--- comments
--------------------------------
-ALTER SEQUENCE comments_id_seq RESTART WITH 6;
-
-INSERT INTO comments(id, content, id_news, id_comment, user_id) VALUES (1, 'Fake news!', 1, NULL, 1);
-INSERT INTO comments(id, content, id_news, id_comment, user_id) VALUES (2, 'Very informative', 2, NULL, 2);
-INSERT INTO comments(id, content, id_news, id_comment, user_id) VALUES (3, 'Loved it!', 2, 1, 3);
-INSERT INTO comments(id, content, id_news, id_comment, user_id) VALUES (4, 'Source?', 3, 2, 4);
-
--------------------------------
--- comment_vote
--------------------------------
-INSERT INTO comment_vote(id_user, id_comment, is_liked) VALUES (1, 1, true);
-INSERT INTO comment_vote(id_user, id_comment, is_liked) VALUES (1, 2, false);
-INSERT INTO comment_vote(id_user, id_comment, is_liked) VALUES (1, 3, true);
-INSERT INTO comment_vote(id_user, id_comment, is_liked) VALUES (2, 2, true);
-INSERT INTO comment_vote(id_user, id_comment, is_liked) VALUES (3, 1, false);
-INSERT INTO comment_vote(id_user, id_comment, is_liked) VALUES (4, 1, true);
-
--------------------------------
--- tag_follow
--------------------------------
-INSERT INTO tag_follow(id_user, id_tag) VALUES (1,1);
-INSERT INTO tag_follow(id_user, id_tag) VALUES (2,2);
-INSERT INTO tag_follow(id_user, id_tag) VALUES (3,3);
-INSERT INTO tag_follow(id_user, id_tag) VALUES (4,4);
-INSERT INTO tag_follow(id_user, id_tag) VALUES (1,4);
-INSERT INTO tag_follow(id_user, id_tag) VALUES (2,5);
-
--------------------------------
 -- tag_proposal
 -------------------------------
 INSERT INTO tag_proposal(tag_name, description, is_handled) VALUES ('Wholesome','I want to tag my cat pictures',false);
@@ -674,8 +589,7 @@ INSERT INTO tag_proposal(tag_name, description, is_handled) VALUES ('Manga','I w
 INSERT INTO tag_proposal(tag_name, description, is_handled) VALUES ('Cars','This important tag is missing',false);
 INSERT INTO tag_proposal(tag_name, description, is_handled) VALUES ('Anime','I want to tag my favorite anime shows without using the "TV" tag', true);
 
----------------------INSERT INTO report(report_type, report_text, is_handled, user_id, id_user, id_news, id_comment) VALUES ('UserReport','User insulted me', false,1,1,NULL, NULL);
-----------
+-------------------------------
 -- tag_proposal_user
 -------------------------------
 INSERT INTO tag_proposal_user(id_user, id_tag) VALUES (1, 1);
@@ -686,21 +600,4 @@ INSERT INTO tag_proposal_user(id_user, id_tag) VALUES (2, 2);
 INSERT INTO tag_proposal_user(id_user, id_tag) VALUES (3, 3);
 INSERT INTO tag_proposal_user(id_user, id_tag) VALUES (4, 3);
 
--------------------------------
--- report --
---'UserReport', 'NewsReport', 'CommentReport'
--------------------------------
-INSERT INTO report(report_type, report_text, is_handled, user_id, id_user, id_news, id_comment) VALUES ('UserReport','User insulted me', false,1,1,NULL, NULL);
-INSERT INTO report(report_type, report_text, is_handled, user_id, id_user, id_news, id_comment) VALUES ('NewsReport','Wrong use of tags', true,2,NULL,2, NULL);
-INSERT INTO report(report_type, report_text, is_handled, user_id, id_user, id_news, id_comment) VALUES ('CommentReport','Offensive comments', false,3,NULL,NULL,1);
-INSERT INTO report(report_type, report_text, is_handled, user_id, id_user, id_news, id_comment) VALUES ('CommentReport','Spam', false,4, NULL, NULL, 2);
-
--------------------------------
--- notification
--- 'NewsVote', 'CommentVote', 'NewsComment'
--------------------------------
-INSERT INTO notification(notification_type, is_viewed, id_user, id_news, id_comment) VALUES ('NewsVote', false, 1, 1, NULL);
-INSERT INTO notification(notification_type, is_viewed, id_user, id_news, id_comment) VALUES ('CommentVote', false, 2, NULL, 3);
-INSERT INTO notification(notification_type, date, is_viewed, id_user, id_news, id_comment) VALUES ('NewsComment', '2022.10.20', true, 2, NULL, 1);
-INSERT INTO notification(notification_type, date, is_viewed, id_user, id_news, id_comment) VALUES ('NewsVote', '2022.10.20', true, 4, 4, NULL);
 
