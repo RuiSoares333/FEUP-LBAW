defe4cd693fb031b703e8f3134a21835c8d90dd0
up202005303 <up202005303@fe.up.pt>
2023-01-03 21:03:22 +0000

    bug fix tag proposal

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -37,6 +37,37 @@ class TagController extends Controller
         return view('pages.tag', ['tag' => $tag, 'news' => $news, 'user' => $user]);
     }
 
+    public function createProposal(Request $request){
+        if (!Auth::check()) return response("Access Denied", 401);
+        $name = $request->input('name');
+        $tag = Tag::where('tag_name', $name)->exists();
+
+        if($tag) return response('Tag Already Exists', 403);
+
+        $tagP = TagProposal::where('tag_name', $name)->first();
+        if($tagP){ //tag proposal exists add to proposal-user
+            $propUser = DB::table('tag_proposal_user')->where('id_tag', $tagP->id)->where('id_user', Auth()->user()->id)->exists();
+            if($propUser) return response('You Alreay made this Proposal', 403);
+
+            DB::table('tag_proposal_user')->insert([
+                'id_user' => Auth()->user()->id,
+                'id_tag' => $tagP->id
+            ]);
+        }
+        else{ //tag doesnt exist, create and add proposal-user
+            $newProp = new TagProposal;
+            $newProp->tag_name = $name;
+            $newProp->save();
+
+            DB::table('tag_proposal_user')->insert([
+                'id_user' => Auth()->user()->id,
+                'id_tag' => $newProp->id
+            ]);
+        }
+
+        return response('Proposal created', 200);
+    }
+
     public function createTag(Request $request){
         if(!Auth::check()) return response("Access Denied", 401);
         if(!(Auth()->user()->is_admin)) return response("Access Denied", 401);
